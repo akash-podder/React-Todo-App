55b8e3f00b59af635b60419a1e8de77c7305d797
AkashPodder <anantaakash.podder@gmail.com>
2025-03-08 19:50:46 +0600

    - `dynamic Metadata` mechanism added in `Next.js`

--- next-js-basic-tutorial/app/posts/[id]/page.js
+++ next-js-basic-tutorial/app/posts/[id]/page.js
@@ -1,6 +1,18 @@
 import { notFound } from "next/navigation";
 import getPost from '@/lib/getPost'
 
+// this "generateMetadata" Function is for Generating "Dynamic Metadata"
+export async function generateMetadata({params}) {
+  
+  const {id} = await params;
+  const post = await getPost(id); // here in this Page, "getPost(id)" API won't be called TWICE, because "Next.js" can Recognize "Duplicate" Api calls... also, here "fetch()" function is Overriden by "Next.js" to Stop "Duplicate" API Calls
+
+  return {
+    title: post.title,
+    description: post.body,
+  }
+}
+
 export default async function Post({params}) {
   
   const {id} = await params;
